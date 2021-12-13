998456535e979fba7c9b5e62bb2bbd21e560879f
JhoanLopez <jhoanlopezclase@gmail.com>
2021-12-13 19:37:53 +0100

    Añadir clase pincipal

---
+++ src/main/java/ejemplogithub/HelloWorld.java
@@ -0,0 +1,16 @@
+package ejemplogithub;
+
+/**
+ * @date 13 dic. 2021
+ * @author Jhoan López
+ * @email Jhoanlopezclase@gmail.com
+ */
+
+public class HelloWorld extends javax.swing.JFrame {
+   public static void main (String [] args) {
+       HelloWorld app = new HelloWorld();
+       app.setSize(300, 200);
+       app.setTitle("Hola mundo");
+       app.setVisible(true);  
+   }
+}
